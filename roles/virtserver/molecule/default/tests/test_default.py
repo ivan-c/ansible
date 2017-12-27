ad1e9bfddb904f42bf826ad1fc0692e82794614b
Ivan Cvitkovic <icvitkovic@gmail.com>
2017-12-26 22:12:10 -0800

    Install apt-cacher-ng on VM hosts

--- roles/virtserver/molecule/default/tests/test_default.py
+++ roles/virtserver/molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from testinfra.utils import ansible_runner
 
 testinfra_hosts = ansible_runner.AnsibleRunner(
@@ -15,3 +16,14 @@ def test_libvirt_running_and_enabled(host):
     libvirtd = host.service("libvirtd")
     assert libvirtd.is_enabled
     assert libvirtd.is_running
+
+
+def test_apt_cacher_ng_running_and_enabled(host):
+    apt_cacher_ng = host.service("apt-cacher-ng")
+    assert apt_cacher_ng.is_enabled
+    assert apt_cacher_ng.is_running
+
+
+def test_apt_cacher_ng_response(host):
+    response = requests.get("http://localhost:3142")
+    assert 'virtual HTTP repository' in response.content
