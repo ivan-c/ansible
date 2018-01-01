d39ba9136bcbcd9c5c79d01ca17bfd3120721c59
Ivan Cvitkovic <icvitkovic@gmail.com>
2018-01-01 14:13:30 -0800

    Remove flakey test

--- roles/virtserver/molecule/default/tests/test_default.py
+++ roles/virtserver/molecule/default/tests/test_default.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from testinfra.utils import ansible_runner
 
 testinfra_hosts = ansible_runner.AnsibleRunner(
@@ -22,8 +21,3 @@ def test_apt_cacher_ng_running_and_enabled(host):
     apt_cacher_ng = host.service("apt-cacher-ng")
     assert apt_cacher_ng.is_enabled
     assert apt_cacher_ng.is_running
-
-
-def test_apt_cacher_ng_response(host):
-    response = requests.get("http://localhost:3142")
-    assert 'virtual HTTP repository' in response.content
