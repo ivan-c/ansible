5a4234b9eddb4806cfb90a8bb896e43a06e8151a
Ivan Cvitkovic <icvitkovic@gmail.com>
2018-01-26 21:23:00 -0800

    Add docstrings, reorder tests

--- roles/virtserver/molecule/default/tests/test_default.py
+++ roles/virtserver/molecule/default/tests/test_default.py
@@ -7,18 +7,8 @@ testinfra_hosts = ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_libvirtd_is_installed(host):
-    libvirt = host.package("libvirt-daemon-system")
-    assert libvirt.is_installed
-
-
-def test_libvirt_running_and_enabled(host):
-    libvirtd = host.service("libvirtd")
-    assert libvirtd.is_enabled
-    assert libvirtd.is_running
-
-
 def test_apt_cacher_ng_running_and_enabled(host):
+    """Test apt-cacher-ng running on VM host"""
     apt_cacher_ng = host.service("apt-cacher-ng")
     assert apt_cacher_ng.is_enabled
     assert apt_cacher_ng.is_running
@@ -29,7 +19,13 @@ def test_apt_cacher_ng_caching_provisioning(host):
     assert host.file("/var/log/apt-cacher-ng/apt-cacher.log").contains("base-installer")
 
 
+def test_libvirtd_is_installed(host):
+    libvirt = host.package("libvirt-daemon-system")
+    assert libvirt.is_installed
+
+
 def test_kvm_device(host):
+    """Test that KVM device has correct ownership and permissions"""
     kvm_device_path = "/dev/kvm"
     assert stat.S_ISCHR(os.stat(kvm_device_path).st_mode)
 
@@ -39,7 +35,14 @@ def test_kvm_device(host):
     assert kvm_device.mode == 0o660
 
 
+def test_libvirt_running_and_enabled(host):
+    libvirtd = host.service("libvirtd")
+    assert libvirtd.is_enabled
+    assert libvirtd.is_running
+
+
 def test_vm_started(host):
+    """Test that guest VM shutdown and restarted gracefully, after provisioning"""
     vars = host.ansible.get_variables()
     domain_log = "/var/log/libvirt/qemu/{inventory_hostname}.log".format(**vars)
 
